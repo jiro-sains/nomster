b945f54d9013395a1986b343abf757d90abc9d05
jiro-sains <jrosains@gmail.com>
2017-02-11 23:31:11 +0000

    photo still not working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,8 +7,10 @@ class PhotosController < ApplicationController
     redirect_to place_path(@place)
   end
 
+  private
+
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:picture, :caption)
   end
 
 end
